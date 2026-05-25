7260f295ab6fbe96dcd2c7827065a285071c1fb5
Sofia Garbazza <sofiagarbazza@gmail.com>
2026-05-25 09:07:39 -0300

    Add files via upload

---
+++ atividade_25.05/calculadora.py
@@ -0,0 +1,33 @@
+import requests
+from flask import Flask, render_template, request
+
+def calcular():
+    num1 = float(request.form['num1'])
+    num2 = float(request.form['num2'])
+    operacao = request.form['operacao']
+    
+
+    if operacao == '+':
+        resultado = num1 + num2
+        etapas = f'{num1} + {num2} = {resultado}'
+    
+    elif operacao == '-':
+        resultado = num1 - num2
+        etapas = f'{num1} - {num2} = {resultado}'
+
+    elif operacao == '*':
+            resultado = num1 * num2
+            etapas = f'{num1} * {num2} = {resultado}'
+    elif operacao == '/':
+     if num2 != 0:
+         resultado = num1 / num2
+         etapas = f'{num1} / {num2} = {resultado}'
+     else:
+        resultado = "Operação inválida"
+        etapas = "Não é possível dividir por zero"
+
+    else:
+        resultado = "Operação inválida"
+        etapas = "A operação selecionada é inválida"
+
+    return render_template('calculadora.html', etapas=etapas, resultados=resultado)
